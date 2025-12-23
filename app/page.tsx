919dfb2f1157fb663aa15bb5c76a3a0351988e6a
haeunj400-hub <haeunj400@gmail.com>
2025-12-23 23:08:12 +0900

    background black 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,7 @@
-<div style={{backgroundColor: 'black',width: '100%', height: '100vh' }}>
+export default function Home(){
+  return(
+    <div style={{backgroundColor: 'black',width: '100%', height: '100vh' }}>
 </div>
+  );
+}
+
