df15277b96c49ebeea0cf3b26ae9136b7eaa81fe
haeunj400-hub <haeunj400@gmail.com>
2025-12-23 23:18:58 +0900

    introduce

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home(){
   return(
-    <div style={{backgroundColor: 'black',width: '100%', height: '100vh' }}>
+    <div style={{backgroundColor: 'black',width: '100%', height: '100vh', color: 'white' }}>
+
+<h1>Hi, I'm Dilip Kumar - Web Developer In Delhi</h1>
+<h2>Custom Web Development Services in Delji NCR</h2>
+<p> I am a freelance web developer based in Delhi, specializing</p>
+<p> in responsive website design, custom web development</p>
+<p> and SEO-optimized solutions. With expertise in Next.js,</p>
+<p> React,PHP,and WordPress,I help businesses in Delhi and</p>
+<p> beyond build high-performing,secure,and user-friendly</p>
+<p> websites.</p>
 </div>
-  );
+);
 }
 
