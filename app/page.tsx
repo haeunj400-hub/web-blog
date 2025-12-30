a38b08983fe996ce3dc10e34e481908b578a6743
haeunj400-hub <haeunj400@gmail.com>
2025-12-30 21:02:07 +0900

    배경색 변경 및 소개 문구

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 export default function Home(){
   return(
-    <div style={{backgroundColor: 'black',width: '100%', height: '100vh', color: 'white' }}>
-
-<h1>Hi, I'm Dilip Kumar - Web Developer In Delhi</h1>
-<h2>Custom Web Development Services in Delji NCR</h2>
-<p> I am a freelance web developer based in Delhi, specializing</p>
-<p> in responsive website design, custom web development</p>
-<p> and SEO-optimized solutions. With expertise in Next.js,</p>
-<p> React,PHP,and WordPress,I help businesses in Delhi and</p>
-<p> beyond build high-performing,secure,and user-friendly</p>
-<p> websites.</p>
-</div>
+    <div className="flex min-h-screen items-center justify-center bg-background">
+     <main className="flex min-h-screen flex-col items-center justify-between py-32 px-16">
+      <div className="flex flex-col items-center py-32 px-32 space-y-6 text-center">
+      <h1 className="text-5xl">Hi, I'm Haeun Cho - Web Developer In South Korea</h1>
+      <h2 className="text-3xl mx-3">Computer Science Student</h2> 
+      <p className="text-base mx-10 mb-10">I am a freelance web developer based in Delhi,<br/>specializing in responsive website design, custom web development, and SEO-optimized solutions.<br/>With expertise in Next.js, React, PHP, and WordPress, I help businesses in Delhi and beyond build high-performing, secure, and user-friendly websites.
+        </p>
+     </div>
+   </main>
+  </div>
 );
 }
 
