c47dea8639aab14f27ade3bc487485f516065d4d
haeunj400-hub <haeunj400@gmail.com>
2026-01-26 18:53:01 +0900

    수정사항 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const quicksand = Quicksand({
 });
 
 export const metadata: Metadata = {
-  title: "Chaeyeong Park | Web",
+  title: "Haeun Cho | Web",
   description: "Computer science student",
 };
 
